b50567d4d2d384ecc85d7fbecec2154a49f00f57
Ronald van Manen <rvanmanen@gmail.com>
2023-04-17 11:18:32 +0200

    Fix Win32 error 206 "The filename or extension is too long" when generating interop code.

--- generation/winnt/generate.ps1
+++ generation/winnt/generate.ps1
@@ -1,1 +1,1 @@
-..\..\..\..\thirdparty\microsoft\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Debug\net6.0\ClangSharpPInvokeGenerator.exe "@settings.rsp"
+& dotnet ClangSharpPInvokeGenerator "@settings.rsp"
